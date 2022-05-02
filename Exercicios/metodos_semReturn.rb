6747c86b00802e1f3425968cac9cb6915af38334
Laylla Rodrigues <62724150+LayllaRodrigues@users.noreply.github.com>
2022-05-02 10:12:38 -0300

    metodos sem return

---
+++ Exercicios/metodos_semReturn.rb
@@ -0,0 +1,10 @@
+
+# Todos os métodos Ruby retornam sempre o resultado da última expressão de- clarada, por esse motivo, você não precisa explicitamente adicionar o return no final de cada método:
+
+def boas_vindas(nome)
+    "Bem vindo: #{nome}"
+end
+p boas_vindas("Lucas") # => "Bem vindo: Lucas"
+
+# Quando invocado o método boas_vindas retornará a String interpolada
+# com o valor do argumento nome que foi passado.
